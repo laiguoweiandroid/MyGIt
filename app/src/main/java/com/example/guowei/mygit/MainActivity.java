7339aec2bdc48d0a56d8a6dc613544600c4db6a9
laiguoweiandroid <877157601@qq.com>
2017-05-31 17:37:18 +0800

    master.

--- app/src/main/java/com/example/guowei/mygit/MainActivity.java
+++ app/src/main/java/com/example/guowei/mygit/MainActivity.java
@@ -6,39 +6,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-  private Button btn_feature1,btn_feature2,btn_feature4;
+  private Button btn_feature;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_feature1 = (Button) findViewById(R.id.feature1);
-        btn_feature2 = (Button) findViewById(R.id.feature2);
-        btn_feature4 = (Button) findViewById(R.id.feature4);
+        btn_feature = (Button) findViewById(R.id.feature1);
     }
-    public void feature1(View view){
+    public void feature(View view){
         if(view.getId()!=0){
-            if(btn_feature1.getText().toString().equals("分支功能1")){
-                btn_feature1.setText("展示分支功能1");
+            if(btn_feature.getText().toString().equals("分支功能1")){
+                btn_feature.setText("展示分支功能1");
             }else {
-                btn_feature1.setText("分支功能1");
-            }
-        }
-    }
-    public void feature2(View view){
-        if(view.getId()!=0){
-            if(btn_feature2.getText().toString().equals("分支功能2")){
-                btn_feature2.setText("展示分支功能2");
-            }else {
-                btn_feature2.setText("分支功能2");
-            }
-        }
-    }
-    public void feature4(View view){
-        if(view.getId()!=0){
-            if(btn_feature4.getText().toString().equals("分支功能4")){
-                btn_feature4.setText("展示分支功能4");
-            }else {
-                btn_feature4.setText("分支功能4");
+                btn_feature.setText("分支功能1");
             }
         }
     }
